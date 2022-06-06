cb42c3707d9908b5c5bb3eeac95a75521be09ee8
SachinBahukhandi <sachinb0013@gmail.com>
2022-06-05 20:00:00 -0600

    Added interpolation

---
+++ src/components/Interpolation.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>{{ state.message }}</h1>
+    </div>
+</template>
+<script>
+import { reactive } from 'vue'
+export default {
+    setup() {
+        const state= reactive({message: 'Hello World!'});
+        return state;
+    },
+}
+</script>
