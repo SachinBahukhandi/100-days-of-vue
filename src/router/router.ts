9fcbf90762e3e8a4c69038ab782b781b4f4efac1
SachinBahukhandi <sachinb0013@gmail.com>
2022-06-07 20:00:00 -0600

    Added class bindings

--- src/router/router.ts
+++ src/router/router.ts
@@ -2,6 +2,8 @@ import Home from '../views/Home.vue';
 import Login from '../views/Login.vue';
 import Interpolation from '../views/Interpolation.vue';
 import Computed from '../views/Computed.vue';
+import Bindings from '../views/Bindings.vue';
+
 export default [
     {
         path: '/home',
@@ -22,5 +24,10 @@ export default [
         path: '/computed',
         name: 'Computed',
         component: Computed,
+    },
+    {
+        path: '/bindings',
+        name: 'Bindings',
+        component: Bindings,
     },
 ];
---
+++ src/views/Bindings.vue
@@ -0,0 +1,21 @@
+<template>
+ <div
+  class="static"
+  :class="{ active: isActive, 'text-danger': hasError }"
+></div>
+</template>
+<script lang="ts">
+import { ref, reactive, computed, defineComponent } from "vue";
+
+const isActive = ref(true);
+const hasError = ref(false);
+
+export default defineComponent({
+  setup() {
+    return {
+      isActive,
+      hasError
+    };
+  },
+});
+</script>
