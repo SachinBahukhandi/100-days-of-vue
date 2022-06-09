e0c943ca61a54bab487266c961c85e5f9dba060e
SachinBahukhandi <sachinb0013@gmail.com>
2022-06-08 20:00:00 -0600

    Modified class bindings

--- src/views/Bindings.vue
+++ src/views/Bindings.vue
@@ -1,20 +1,39 @@
 <template>
  <div
   class="static"
-  :class="{ active: isActive, 'text-danger': hasError }"
-></div>
+>
+<h1>Current State: {{ state.active?"Active":"Inactive" }}</h1>
+<button type="button" @click="toggleError">Set to: {{ state.activeText }}</button>
+</div>
 </template>
 <script lang="ts">
 import { ref, reactive, computed, defineComponent } from "vue";
 
-const isActive = ref(true);
-const hasError = ref(false);
+
+
+const state= reactive({
+  active: true,
+  error: false,
+  activeText: "Inactive",
+  errorText: "Error"
+});
+
+function toggleBtn(){
+  state.error= !state.error;
+  state.errorText= state.error? "Error":"No Error";
+}
+function toggleError(){
+  state.active= !state.active;
+  console.log(state.active);
+  state.activeText= state.active== true? "Inactive":"Active";
+}
 
 export default defineComponent({
   setup() {
     return {
-      isActive,
-      hasError
+      state,
+      toggleBtn,
+      toggleError
     };
   },
 });
