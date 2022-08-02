75a1944c9624a3504c079765d228de6e264926b0
SachinBahukhandi <sachinb0013@gmail.com>
2022-08-02 23:20:26 +0530

    Added the custom font

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container grid-container grid grid-cols-5">
+        <div class="item1 col-span-4 text-center mt-2 p-2">
+            <h4>Stackoverflow.com</h4>
+        </div>
+        <div class="item2 col-span-1">
+            <p>Stackoverflow.com</p>
+        </div>
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    setup() {
+
+    },
+})
+</script>
