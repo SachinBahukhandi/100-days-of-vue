7f74a6183e71e64fd4ae51079fc09c6eb3d2e7f0
SachinBahukhandi <sachinb0013@gmail.com>
2022-08-07 23:59:03 +0530

    Added tabs

---
+++ src/components/Tab.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul>
+    <li
+      v-for="item in items" :key="item.key"
+      class="-mb-px mr-2 last:mr-0 flex-auto text-center"
+    >
+      <a
+        class="text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal"
+        @click="toggleTabs(1)"
+        :class="{
+          'text-red-600 bg-white': openTab !== 1,
+          'text-white bg-red-600': openTab === 1,
+        }"
+      >
+        Profile
+      </a>
+    </li>
+  </ul>
+</template>
